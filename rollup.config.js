59c907ac7c5356f9b76f80e82de2bcb6a8e0e49f
Emerson452 <emerson452pro@gmail.com>
2023-02-28 12:18:07 +0100

    :sparkles: :package: compile component with rollup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+
+export default {
+  input: "src/components/index.ts",
+  output: {
+    dir: "dist",
+    format: "cjs",
+    sourcemap: true,
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    postcss({
+      extract: true,
+      modules: true,
+    }),
+  ],
+  external: ["react", "react-dom"],
+};

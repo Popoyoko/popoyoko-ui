752569e6ebd2b7a31b5213d3b01a331a7c78ac51
Emerson452 <emerson452pro@gmail.com>
2023-02-28 12:31:11 +0100

    :sparkles: :rotating_light: improve and fix the compile

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from "rollup-plugin-typescript2";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
+import copy from "rollup-plugin-copy";
 
 export default {
   input: "src/components/index.ts",
@@ -17,6 +18,11 @@ export default {
     postcss({
       extract: true,
       modules: true,
+    }),
+    copy({
+      targets: [
+        { src: "src/components/**/*", dest: "dist/components" },
+      ],
     }),
   ],
   external: ["react", "react-dom"],

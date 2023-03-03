6d69bc479cd6d4961e8880d9e9598f586f384205
Emerson452 <emerson452pro@gmail.com>
2023-03-03 11:37:28 +0100

    :wrench: add rollup config and set the scripts

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  external: ['react', 'react-dom'],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+    production && terser(),
+  ],
+};

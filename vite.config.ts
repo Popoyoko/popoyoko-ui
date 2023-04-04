944a830f2c5c10ff03a31658dfd728afd36ac5bc
Emerson452 <emerson452pro@gmail.com>
2023-04-04 14:13:50 +0200

    ✨ 🚨 add a linter with eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";//SWC (Speedy WebAssembly Compiler)
 import copy from "rollup-plugin-copy";
+import eslint from '@rollup/plugin-eslint';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
+    eslint({
+      include: ['src/**/*.ts', 'src/**/*.tsx'], // Les fichiers à linter
+      exclude: ['node_modules/**', 'dist/**'] // Les fichiers à exclure
+    }),
     // ajoutez le plugin copy et configurez-le pour copier le fichier package.json
     copy({
       targets: [{ src: "package.json", dest: "dist" },{ src: 'src/components/**/*', dest: 'dist/' }],

c71c2f88af3b3fcf9d5574fc208f0149d092f0fe
Emerson452 <emerson452pro@gmail.com>
2023-04-04 10:50:23 +0200

    🔧 👷

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";//SWC (Speedy WebAssembly Compiler)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    //vider le répertoire de sortie avant de générer
+    emptyOutDir: true,
+    rollupOptions: {
+      input: "./src/components/",
+      output: {
+        // nom des fichiers générés pour les points d'entrée
+        entryFileNames: "[name].js",
+        // nom des fichiers générés pour les "chunks"
+        chunkFileNames: "[name].js",
+        //nom des fichiers générés pour les fichiers statiques 
+        assetFileNames: "[name].[ext]",
+      }
+    },
+  },
+});

d25323e132839fca5921cc361d4d87c2e4b091e1
Emerson452 <emerson452pro@gmail.com>
2023-04-19 18:02:44 +0200

    ✨ Create somes stories for the card component

---
+++ build/stories/Card/Card.d.ts
@@ -0,0 +1,11 @@
+import "./Card.css";
+interface CardProps {
+    mention?: string;
+    label?: string;
+    subTitle?: string;
+    price?: number;
+    background?: string;
+    variant?: "simple" | "media";
+}
+export declare const Card: ({ background, label, mention, subTitle, price, ...props }: CardProps) => JSX.Element;
+export {};

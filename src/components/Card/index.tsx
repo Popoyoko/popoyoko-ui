f6a4103840d148c49ca089d235d74187430102de
Emerson452 <emerson452pro@gmail.com>
2023-04-19 12:05:41 +0200

    🚧 imrove the card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,24 +2,22 @@ import "./index.css";
 import React from "react";
 
 interface CardProps {
-    label: string;
-    background?: string;
-    variant?: "simple" | "media";
+  label?: string;
+  background?: string;
+  variant?: "simple" | "media";
 }
 
 const Card = ({
-  variant = "simple",
   background,
   label,
   ...props
 }: CardProps) => {
-  const cardClass = variant === "simple" ? "card" : "card-media";
+  const variant = label ? "media" : "simple";
+  const cardClass = variant === "simple" ? "card" : "card media";
   return (
-    <div 
-    className={cardClass} 
-    style={{ background }} 
-    {...props}>
-    {label}
+    <div className="card">
+      <div className={cardClass} style={{ background }} {...props}></div>
+      {label && <div className="card-label">{label}</div>}
     </div>
   );
 };

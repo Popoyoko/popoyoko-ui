51163ddca33772ee9e4094a49c0e4473a946dc44
Emerson452 <emerson452pro@gmail.com>
2023-04-20 19:36:07 +0200

    ⬆️ mise a jour stories 7.0

--- src/stories/Button/Button.stories.tsx
+++ src/stories/Button/Button.stories.tsx
@@ -8,9 +8,9 @@ const meta: Meta<typeof Button> = {
   title: 'Design System/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  // argTypes: {
-  //   backgroundColor: { control: 'color' },
-  // },
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 } 
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args

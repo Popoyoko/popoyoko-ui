ea0ba923f323a7d012696396048b4e9ba6d42a24
Emerson452 <emerson452pro@gmail.com>
2023-05-02 16:25:41 +0200

    🐛 debug changement color in storybook

--- src/stories/Information/Information.stories.tsx
+++ src/stories/Information/Information.stories.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Information } from './Information'
+import { Information } from "./Information";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Information> = {
-  title: 'Design System/Information',
+  title: "Design System/Information",
   component: Information,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    background: { control: 'color' },
+    background: { control: "color" },
   },
-} 
+};
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export default meta;
-type Story = StoryObj<typeof Information>
+type Story = StoryObj<typeof Information>;
 
 export const Primary: Story = {
-  render: () => <Information label='Exemple of really long texte that can be fit there' />,
+  render: (args) => <Information {...args} />,
+  args: {
+    label: "Exemple of really long texte that can be fit there",
+  },
 };
-
--- storybook-static/assets/DocsRenderer-7FRJXR4N-af0d761d.js
+++
@@ -1,2 +0,0 @@
-import{D as f,d as D}from"./chunk-PCJTTTQV-e57dfcea.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./iframe-68c40918.js";import"../sb-preview/runtime.mjs";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./doctrine-5b61ce67.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{f as DocsRenderer,D as defaultComponents};
-//# sourceMappingURL=DocsRenderer-7FRJXR4N-af0d761d.js.map

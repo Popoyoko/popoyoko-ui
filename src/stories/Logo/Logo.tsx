c95ea6ffbe62c088fa1cfaf1407736f4ba91afb7
Emerson452 <emerson452pro@gmail.com>
2023-05-02 13:18:17 +0200

    ✨ import Logo component

---
+++ src/stories/Logo/Logo.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import React from 'react';
+
+interface LogoProps {
+    src: string;
+}
+
+export const Logo = ({
+    src
+}: LogoProps) => {
+    return (
+        <div className="logo">
+            <img src={src}></img>
+        </div>
+    );
+};

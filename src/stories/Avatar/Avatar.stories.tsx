293df6159d84ae908dbd13e33f715390ade6bf33
Emerson452 <emerson452pro@gmail.com>
2023-05-04 11:25:10 +0200

    ✨ 📦 Stories for Avatar and rebuild

---
+++ src/stories/Avatar/Avatar.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Avatar } from "./Avatar";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Avatar> = {
+  title: "Design System/Avatar",
+  component: Avatar,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    small: {
+      control: "boolean",
+      table: {
+        category: "Variant",
+      },
+    },
+    background: {
+      control: "file",
+      table: {
+        category: "Background",
+      },
+    },
+  },
+  args: {
+    background: "#808080",
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export default meta;
+type Story = StoryObj<typeof Avatar>;
+
+export const small: Story = {
+    render: () => <Avatar small/>,
+};
+
+export const cover: Story = {
+  render: () => <Avatar />,
+};

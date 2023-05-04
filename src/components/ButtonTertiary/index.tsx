aecb58c52877afb3575d3783dfb6c9bae5020be5
Emerson452 <emerson452pro@gmail.com>
2023-05-04 12:40:14 +0200

    ✨ make button tertiary component

---
+++ src/components/ButtonTertiary/index.tsx
@@ -0,0 +1,51 @@
+import './index.css';
+import React from 'react';
+
+interface ButtonTertiaryProps {
+    typeSvg: 'none' | 'left' | 'right' | 'only';
+    label: string;
+    srcIcon?: string;
+    onClick?: () => void;
+}
+
+const ButtonTertiary = ({
+    label,
+    typeSvg,
+    srcIcon,
+    onClick,
+}: ButtonTertiaryProps) => {
+
+    if(typeSvg === 'none'){
+        return(
+            <button className='buttonTertiary'>
+                {label}
+            </button>
+        );
+    } else if(typeSvg === 'left'){
+        return(
+            <button className='buttonTertiary'>
+                <img src={srcIcon}></img>
+                {label}
+            </button>
+        );
+    } else if(typeSvg === 'right'){
+        return(
+            <button className='buttonTertiary'>
+                {label}
+                <img src={srcIcon}></img>
+                </button>
+        );
+    } else if(typeSvg === 'only'){
+        return(
+            <button className='buttonTertiary'>
+                <img src={srcIcon}></img>
+            </button>
+        );
+    }
+
+    return (
+        <button className='buttonTertiary'></button>
+    )
+};
+
+export default ButtonTertiary;

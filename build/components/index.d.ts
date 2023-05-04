d2776ff936a639ba72d53336b0057c4b3452b8eb
Emerson452 <emerson452pro@gmail.com>
2023-05-04 11:45:19 +0200

    ⚰️ remove example files

--- build/components/index.d.ts
+++ build/components/index.d.ts
@@ -1,6 +1,6 @@
-export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as Information } from "./Information";
 export { default as Text } from "./Text";
 export { default as Logo } from "./Logo";
 export { default as Icon } from "./Icon";
+export { default as Avatar } from "./Avatar";
--- build/popoyoko-ui-vite.es.js
+++ build/popoyoko-ui-vite.es.js
@@ -1,38 +1,33 @@
 import e from "react";
-const d = ({ primary: t = !1, size: a = "medium", backgroundColor: n, label: i, ...c }) => {
-  const o = t ? "storybook-button--primary" : "storybook-button--secondary";
-  return /* @__PURE__ */ e.createElement("button", {
-    type: "button",
-    className: ["storybook-button", `storybook-button--${a}`, o].join(" "),
-    style: { backgroundColor: n },
-    ...c
-  }, i);
+const d = ({ background: t, backgroundColor: i, label: a, mention: M, subTitle: c, price: n, ...I }) => {
+  const l = (a ? "media" : "simple") === "simple" ? "card" : "card media";
+  return /* @__PURE__ */ e.createElement("div", { className: "card" }, /* @__PURE__ */ e.createElement("div", { className: l, style: { background: t }, ...I }), a && /* @__PURE__ */ e.createElement("div", { className: "card-section", style: { backgroundColor: i } }, /* @__PURE__ */ e.createElement("div", { className: "card-group" }, /* @__PURE__ */ e.createElement("p", null, M), /* @__PURE__ */ e.createElement("div", { className: "card-information" }, /* @__PURE__ */ e.createElement("div", { className: "card-title" }, /* @__PURE__ */ e.createElement("p", null, a), /* @__PURE__ */ e.createElement("p", null, c)), n))));
 };
-const N = ({ background: t, backgroundColor: a, label: n, mention: i, subTitle: c, price: o, ...m }) => {
-  const s = (n ? "media" : "simple") === "simple" ? "card" : "card media";
-  return /* @__PURE__ */ e.createElement("div", { className: "card" }, /* @__PURE__ */ e.createElement("div", { className: s, style: { background: t }, ...m }), n && /* @__PURE__ */ e.createElement("div", { className: "card-section", style: { backgroundColor: a } }, /* @__PURE__ */ e.createElement("div", { className: "card-group" }, /* @__PURE__ */ e.createElement("p", null, i), /* @__PURE__ */ e.createElement("div", { className: "card-information" }, /* @__PURE__ */ e.createElement("div", { className: "card-title" }, /* @__PURE__ */ e.createElement("p", null, n), /* @__PURE__ */ e.createElement("p", null, c)), o))));
-};
-const r = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNCIgaGVpZ2h0PSIxNyIgdmlld0JveD0iMCAwIDQgMTciIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0yIDRDMy4xMDQ1NyA0IDQgMy4xMDQ1NyA0IDJDNCAwLjg5NTQzMSAzLjEwNDU3IDAgMiAwQzAuODk1NDMxIDAgMCAwLjg5NTQzMSAwIDJDMCAzLjEwNDU3IDAuODk1NDMxIDQgMiA0WiIgZmlsbD0iI0VERjFGQSIvPgo8cGF0aCBkPSJNNCAxN1Y2SDBWMTdINFoiIGZpbGw9IiNFREYxRkEiLz4KPC9zdmc+Cg==", g = ({
+const m = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNCIgaGVpZ2h0PSIxNyIgdmlld0JveD0iMCAwIDQgMTciIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0yIDRDMy4xMDQ1NyA0IDQgMy4xMDQ1NyA0IDJDNCAwLjg5NTQzMSAzLjEwNDU3IDAgMiAwQzAuODk1NDMxIDAgMCAwLjg5NTQzMSAwIDJDMCAzLjEwNDU3IDAuODk1NDMxIDQgMiA0WiIgZmlsbD0iI0VERjFGQSIvPgo8cGF0aCBkPSJNNCAxN1Y2SDBWMTdINFoiIGZpbGw9IiNFREYxRkEiLz4KPC9zdmc+Cg==", r = ({
   background: t,
-  label: a
+  label: i
   // ...props
-}) => /* @__PURE__ */ e.createElement("div", { className: "container-information", style: { background: t } }, /* @__PURE__ */ e.createElement("h2", null, "Information"), /* @__PURE__ */ e.createElement("div", { className: "information-section" }, /* @__PURE__ */ e.createElement("div", { className: "information-svg" }, /* @__PURE__ */ e.createElement("img", { src: r, alt: "Information" })), /* @__PURE__ */ e.createElement("div", { className: "information-content" }, a)));
-const D = ({ title: t, description: a, onChange: n }) => /* @__PURE__ */ e.createElement("div", { className: "text-layout" }, /* @__PURE__ */ e.createElement("p", { className: "text-title" }, t), /* @__PURE__ */ e.createElement("textarea", {
-  value: a,
-  onChange: n,
+}) => /* @__PURE__ */ e.createElement("div", { className: "container-information", style: { background: t } }, /* @__PURE__ */ e.createElement("h2", null, "Information"), /* @__PURE__ */ e.createElement("div", { className: "information-section" }, /* @__PURE__ */ e.createElement("div", { className: "information-svg" }, /* @__PURE__ */ e.createElement("img", { src: m, alt: "Information" })), /* @__PURE__ */ e.createElement("div", { className: "information-content" }, i)));
+const E = ({ title: t, description: i, onChange: a }) => /* @__PURE__ */ e.createElement("div", { className: "text-layout" }, /* @__PURE__ */ e.createElement("p", { className: "text-title" }, t), /* @__PURE__ */ e.createElement("textarea", {
+  value: i,
+  onChange: a,
   className: "text-description"
 }));
-const E = ({ src: t }) => /* @__PURE__ */ e.createElement("div", { className: "logo" }, /* @__PURE__ */ e.createElement("img", { src: t }));
-const l = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTE2IDE2QzE5LjMxMzcgMTYgMjIgMTMuMzEzNyAyMiAxMEMyMiA2LjY4NjI5IDE5LjMxMzcgNCAxNiA0QzEyLjY4NjMgNCAxMCA2LjY4NjI5IDEwIDEwQzEwIDEzLjMxMzcgMTIuNjg2MyAxNiAxNiAxNloiIGZpbGw9IiMzMjU2OEYiLz4KPHBhdGggZD0iTTQgMjhDNCAyMy4wMjk0IDkuMzcyNTggMTkgMTYgMTlDMjIuNjI3NCAxOSAyOCAyMy4wMjk0IDI4IDI4SDRaIiBmaWxsPSIjMzI1NjhGIi8+Cjwvc3ZnPgo=", y = ({ small: t, medium: a, large: n }) => {
-  let i = "icon";
-  return t && (i += " icon-small"), a && (i += " icon-medium"), n && (i += " icon-large"), /* @__PURE__ */ e.createElement("div", { className: i }, /* @__PURE__ */ e.createElement("img", { src: l, alt: "Icon" }));
+const o = ({ src: t }) => /* @__PURE__ */ e.createElement("div", { className: "logo" }, /* @__PURE__ */ e.createElement("img", { src: t }));
+const g = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTE2IDE2QzE5LjMxMzcgMTYgMjIgMTMuMzEzNyAyMiAxMEMyMiA2LjY4NjI5IDE5LjMxMzcgNCAxNiA0QzEyLjY4NjMgNCAxMCA2LjY4NjI5IDEwIDEwQzEwIDEzLjMxMzcgMTIuNjg2MyAxNiAxNiAxNloiIGZpbGw9IiMzMjU2OEYiLz4KPHBhdGggZD0iTTQgMjhDNCAyMy4wMjk0IDkuMzcyNTggMTkgMTYgMTlDMjIuNjI3NCAxOSAyOCAyMy4wMjk0IDI4IDI4SDRaIiBmaWxsPSIjMzI1NjhGIi8+Cjwvc3ZnPgo=", j = ({ small: t, medium: i, large: a }) => {
+  let M = "icon";
+  return t && (M += " icon-small"), i && (M += " icon-medium"), a && (M += " icon-large"), /* @__PURE__ */ e.createElement("div", { className: M }, /* @__PURE__ */ e.createElement("img", { src: g, alt: "Icon" }));
+};
+const s = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTkiIGhlaWdodD0iMTUiIHZpZXdCb3g9IjAgMCAxOSAxNSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTExLjU2MzMgMEg1LjQzNjc0TDMuNzcwMDcgMi43NUgwVjEzSDRWMTFIMlY0Ljc1SDQuODk2NTlMNi41NjMyNiAySDEwLjQzNjdMMTIuMTAzNCA0Ljc1SDE1VjdIMTdWMi43NUgxMy4yMjk5TDExLjU2MzMgMFoiIGZpbGw9IiMxQjYwNDIiLz4KPHBhdGggZD0iTTE1IDE1VjEzSDEzVjExSDE1VjlIMTdWMTFIMTlWMTNIMTdWMTVIMTVaIiBmaWxsPSIjMUI2MDQyIi8+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNMTMgOC41QzEzIDEwLjk4NTMgMTAuOTg1MyAxMyA4LjUgMTNDNi4wMTQ3MiAxMyA0IDEwLjk4NTMgNCA4LjVDNCA2LjAxNDcyIDYuMDE0NzIgNCA4LjUgNEMxMC45ODUzIDQgMTMgNi4wMTQ3MiAxMyA4LjVaTTExIDguNUMxMSA5Ljg4MDcxIDkuODgwNzEgMTEgOC41IDExQzcuMTE5MjkgMTEgNiA5Ljg4MDcxIDYgOC41QzYgNy4xMTkyOSA3LjExOTI5IDYgOC41IDZDOS44ODA3MSA2IDExIDcuMTE5MjkgMTEgOC41WiIgZmlsbD0iIzFCNjA0MiIvPgo8L3N2Zz4K", A = ({ small: t, background: i }) => {
+  let a = "avatar";
+  return t && (a += " avatar-small"), /* @__PURE__ */ e.createElement("div", { className: a, style: { background: i } }, t ? /* @__PURE__ */ e.createElement("div", { className: "badge" }) : /* @__PURE__ */ e.createElement("div", { className: "edit" }, /* @__PURE__ */ e.createElement("button", { className: "btn-edit" }, /* @__PURE__ */ e.createElement("p", null, "Edit"), /* @__PURE__ */ e.createElement("img", { src: s, alt: "edit" }))));
 };
 export {
-  d as Button,
-  N as Card,
-  y as Icon,
-  g as Information,
-  E as Logo,
-  D as Text
+  A as Avatar,
+  d as Card,
+  j as Icon,
+  r as Information,
+  o as Logo,
+  E as Text
 };
 //# sourceMappingURL=popoyoko-ui-vite.es.js.map

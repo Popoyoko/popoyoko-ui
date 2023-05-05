7428d689070c1f6b8ee63231eae23dc95211b09d
Emerson452 <emerson452pro@gmail.com>
2023-05-05 14:23:03 +0200

    ✨ Make stories for ratio 2:3

---
+++ src/stories/RatioRectangle/RatioRectangle.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { RatioRectangle } from "./RatioRectangle";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof RatioRectangle> = {
+  title: "Design System/Ratios/Ratio 2:3",
+  component: RatioRectangle,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export default meta;
+type Story = StoryObj<typeof RatioRectangle>;
+
+export const Ratio2_3: Story = {
+  render: () => <RatioRectangle />,
+};
---
+++ src/stories/RatioRectangle/RatioRectangle.tsx
@@ -0,0 +1,7 @@
+import "./RatioRectangle.css";
+import React from "react";
+
+export const RatioRectangle = ({}) => {
+  return <div className="ratio ratio-2-3"></div>;
+};
+

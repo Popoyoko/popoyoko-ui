87589bd8b06c067d8c9a02d4c61f0724ba089490
Emerson452 <emerson452pro@gmail.com>
2023-05-05 11:09:15 +0200

    ✨ Create stories for section

---
+++ storybook-static/assets/Section.stories-b4619943.js
@@ -0,0 +1,7 @@
+import{a,j as e}from"./jsx-runtime-670450c2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";const l=""+new URL("section-48c20935.svg",import.meta.url).href,r=({title:n,label:o})=>a("div",{className:"container-section",children:[e("h2",{children:n}),a("div",{className:"section-btn",children:[e("div",{className:"section-svg",children:e("img",{src:l,alt:"section"})}),e("p",{children:o})]})]});try{r.displayName="Section",r.__docgenInfo={description:"",displayName:"Section",props:{title:{defaultValue:null,description:"",name:"title",required:!1,type:{name:"string"}},label:{defaultValue:null,description:"",name:"label",required:!1,type:{name:"string"}}}}}catch{}const u={title:"Design System/Section",component:r,args:{title:"Section name",label:"Swap me"}},t={render:()=>e(r,{})};var s,i,c;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => <Section />
+  // args: {
+  //   label: "Exemple of really long texte that can be fit there",
+  // },
+}`,...(c=(i=t.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const g=["Primary"];export{t as Primary,g as __namedExportsOrder,u as default};
+//# sourceMappingURL=Section.stories-b4619943.js.map

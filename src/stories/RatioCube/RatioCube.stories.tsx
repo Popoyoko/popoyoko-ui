d0afbf7fd0bdaffb5a5e27ae49a664f877d52f2b
Emerson452 <emerson452pro@gmail.com>
2023-05-05 14:26:51 +0200

    ✨ Make stories for ratio 1:1

---
+++ src/stories/RatioCube/RatioCube.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { RatioCube } from "./RatioCube";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof RatioCube> = {
+  title: "Design System/Ratios/Ratio 1:1",
+  component: RatioCube,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export default meta;
+type Story = StoryObj<typeof RatioCube>;
+
+export const Ratio1_1: Story = {
+  render: () => <RatioCube />,
+};

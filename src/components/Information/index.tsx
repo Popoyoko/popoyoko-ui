b5504910f1949176ba32e2d0d7495af282125339
Emerson452 <emerson452pro@gmail.com>
2023-05-05 15:31:04 +0200

    🎨 remove background from information component

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -4,17 +4,14 @@ import information from "../../assets/information.svg";
 
 interface InformationProps {
   label?: string;
-  background?: string;
 }
 
 const Information = ({
-  background,
   label,
-  // ...props
 }: InformationProps) => {
 
   return (
-<div className="container-information" style={{ background }}>
+<div className="container-information">
     <h2>Information</h2>
     <div className="information-section" >
         <div className="information-svg"><img src = {information} alt="Information"/></div>
--- storybook-static/assets/Information.stories-5da4c0cb.js
+++
@@ -1,7 +0,0 @@
-import{a as o,j as e}from"./jsx-runtime-670450c2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";const c=""+new URL("information-41dc09fb.svg",import.meta.url).href,a=({background:r,label:l,...m})=>o("div",{className:"container-information",children:[e("h2",{children:"Information"}),o("div",{className:"information-section",style:{background:r},children:[e("div",{className:"information-svg",children:e("img",{src:c,alt:"Information"})}),e("div",{className:"information-content",children:l})]})]});try{a.displayName="Information",a.__docgenInfo={description:"",displayName:"Information",props:{label:{defaultValue:null,description:"",name:"label",required:!1,type:{name:"string"}},background:{defaultValue:null,description:"",name:"background",required:!1,type:{name:"string"}}}}}catch{}const g={title:"Design System/Information",component:a,argTypes:{background:{control:"color"}},args:{label:"Exemple of really long texte that can be fit there"}},n={render:r=>e(a,{...r})};var t,i,s;n.parameters={...n.parameters,docs:{...(t=n.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: args => <Information {...args} />
-  // args: {
-  //   label: "Exemple of really long texte that can be fit there",
-  // },
-}`,...(s=(i=n.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};const u=["Primary"];export{n as Primary,u as __namedExportsOrder,g as default};
-//# sourceMappingURL=Information.stories-5da4c0cb.js.map

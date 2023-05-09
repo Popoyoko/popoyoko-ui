d4d10accce4d6c258f448db32cce6e672a4bd2cf
Emerson452 <emerson452pro@gmail.com>
2023-05-09 17:43:31 +0200

    🚚 🐛 Change path of icon assets

--- src/stories/Icons/Information/informationIcon.tsx
+++ src/stories/Icons/Information/informationIcon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import information from "../../assets/information.svg";
+import information from "../../../assets/information.svg";
 
 export const InformationIcon = () => {
   return <img src={information} alt="Information" />;

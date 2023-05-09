9e2ba52e3900190880bbcaddf84deb22bf037e6a
Emerson452 <emerson452pro@gmail.com>
2023-05-09 23:41:27 +0200

    🎨 Create upload icon component

---
+++ src/Icons/Vector/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Upload from "../../assets/section.svg";
+
+const UploadIcon = () => {
+  return <img src={Upload} alt="Upload" />;
+};
+
+export default UploadIcon;

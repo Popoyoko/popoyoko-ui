52642bc36f09dd43a3f0faf8531f5d8de292e4eb
Emerson452 <emerson452pro@gmail.com>
2023-05-09 17:41:32 +0200

    ✨ make a stories for icon information

---
+++ src/stories/Icons/Information/informationIcon.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import information from "../../assets/information.svg";
+
+export const InformationIcon = () => {
+  return <img src={information} alt="Information" />;
+};
+

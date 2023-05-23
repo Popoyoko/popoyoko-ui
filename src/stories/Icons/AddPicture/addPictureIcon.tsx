81bfcd7532f21a1624ce7e9560b23863e8a5cee1
Emerson452 <emerson452pro@gmail.com>
2023-05-23 16:13:19 +0200

    ✨ AddPicture Icon stories

---
+++ src/stories/Icons/AddPicture/addPictureIcon.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import addPicture from "../../../assets/addPicture.svg";
+
+export const AddPictureIcon = () => {
+  return <img src={addPicture} alt="AddPicture" />;
+};
+
+

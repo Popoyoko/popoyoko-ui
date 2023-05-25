ae702a5f1546c2c49107a25335ebddab20e60cba
Emerson452 <emerson452pro@gmail.com>
2023-05-25 14:14:32 +0200

    🎨 ArrowTriangle icon component

---
+++ src/Icons/ArrowTriangle/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import arrowTriangle from "../../assets/triangle.svg";
+
+const ArrowTriangleIcon = () => {
+  return <img src={arrowTriangle} alt="ArrowTriangle" />;
+};
+
+export default ArrowTriangleIcon;

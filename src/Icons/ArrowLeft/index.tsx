10daa81991baa5a9059c4eb5f68ba5f25869e0ca
Emerson452 <emerson452pro@gmail.com>
2023-05-25 11:24:07 +0200

    🎨 ArrowLeft icon component

---
+++ src/Icons/ArrowLeft/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import arrowLeft from "../../assets/left.svg";
+
+const ArrowLeftIcon = () => {
+  return <img src={arrowLeft} alt="ArrowLeft" />;
+};
+
+export default ArrowLeftIcon;

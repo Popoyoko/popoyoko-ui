51e2378ada25a32ce1700a2b405225b1d5e0b288
Emerson452 <emerson452pro@gmail.com>
2023-05-25 11:18:31 +0200

    🎨 ArrowBottom icon component

---
+++ src/Icons/ArrowBottom/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import arrowBottom from "../../assets/bottom.svg";
+
+const ArrowBottomIcon = () => {
+  return <img src={arrowBottom} alt="ArrowBottom" />;
+};
+
+export default ArrowBottomIcon;

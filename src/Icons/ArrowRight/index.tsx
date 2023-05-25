c28b8566be21a7de081dc6479b7480a962d34a8d
Emerson452 <emerson452pro@gmail.com>
2023-05-25 14:00:14 +0200

    🎨 ArrowRight icon component

---
+++ src/Icons/ArrowRight/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import arrowRight from "../../assets/right.svg";
+
+const ArrowRightIcon = () => {
+  return <img src={arrowRight} alt="ArrowRight" />;
+};
+
+export default ArrowRightIcon;

34981f3fac7060191367fabbe3629c174e42083c
Emerson452 <emerson452pro@gmail.com>
2023-05-25 14:19:56 +0200

    ✨ ArrowTriangle icon stories

---
+++ src/stories/Icons/ArrowTriangle/arrowTriangleIcon.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import arrowTriangle from "../../../assets/triangle.svg";
+
+export const ArrowTriangleIcon = () => {
+  return <img src={arrowTriangle} alt="ArrowTriangle" />;
+};
+

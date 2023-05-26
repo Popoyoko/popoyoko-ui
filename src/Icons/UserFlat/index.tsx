73be719e99b1ee84c3f7dfe0315c36ad83be70b9
Emerson452 <emerson452pro@gmail.com>
2023-05-26 11:06:08 +0200

    🎨 UserFlat icon component

---
+++ src/Icons/UserFlat/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserFlat from "../../assets/userFlat.svg";
+
+const UserFlatIcon = () => {
+  return <img src={UserFlat} alt="UserFlat" />;
+};
+
+export default UserFlatIcon;

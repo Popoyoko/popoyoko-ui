0ede85b198237d62718b095525e59c09d349c78a
Emerson452 <emerson452pro@gmail.com>
2023-05-26 11:09:23 +0200

    🎨 UserLine icon component

---
+++ src/Icons/UserLine/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserLine from "../../assets/userLine.svg";
+
+const UserLineIcon = () => {
+  return <img src={UserLine} alt="UserLine" />;
+};
+
+export default UserLineIcon;

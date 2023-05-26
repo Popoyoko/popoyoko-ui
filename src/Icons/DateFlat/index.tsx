24368a91c6b3ce75cd502b14e9c8d557bdebc2fb
Emerson452 <emerson452pro@gmail.com>
2023-05-26 11:12:20 +0200

    🎨 DateFlat icon component

---
+++ src/Icons/DateFlat/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DateFlat from "../../assets/dateFlat.svg";
+
+const DateFlatIcon = () => {
+  return <img src={DateFlat} alt="DateFlat" />;
+};
+
+export default DateFlatIcon;

acd19bfe72cd205f79a81bd5ff4f6580973b0936
Emerson452 <emerson452pro@gmail.com>
2023-06-05 11:44:01 +0200

    ✨ 🎨 avatar component in relation to subcomponents

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Small from "./Small";
+import Cover from "./Cover";
+
+interface AvatarProps {
+    children?: React.ReactNode;
+}
+
+interface AvatarComponent extends React.FC<AvatarProps> {
+  Small: typeof Small;
+  Cover: typeof Cover;
+}
+
+const Avatar: AvatarComponent = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+Avatar.Small = Small;
+Avatar.Cover = Cover;
+
+export default Avatar;

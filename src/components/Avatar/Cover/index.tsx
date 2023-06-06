2deb94fded390225efffc5fb184da831ec9b025c
Emerson452 <emerson452pro@gmail.com>
2023-06-06 19:16:47 +0200

    🚸 Change background props in avatar component to url

--- src/components/Avatar/Cover/index.tsx
+++ src/components/Avatar/Cover/index.tsx
@@ -8,9 +8,10 @@ interface CoverProps {
 }
 
 const Cover = ({ showedit, background }: CoverProps) => {
+  const backgroundImageStyle = background ? { backgroundImage: `url(${background})` } : {backgroundImage: `url(https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg)`};
 
   return (
-    <div className="cover" style={{ background }}>
+    <div className="cover" style={backgroundImageStyle}>
         {showedit && (
         <div className="edit">
             <button className="btn-edit">

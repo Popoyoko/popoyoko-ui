9301f9441fc2a2db0ad14e75ca8a9c912e1be9d2
JulienBNT <julien.binet91@gmail.com>
2023-06-07 15:10:50 +0200

    :sparkles: input search added to components

---
+++ src/components/Inputs/Search/index.tsx
@@ -0,0 +1,30 @@
+import './index.css';
+import React from 'react';
+
+interface InputSearchProps {
+    placeholder: string;
+    srcIcon: string;
+};
+
+const InputSearch = ({
+    placeholder,
+    srcIcon,
+}: InputSearchProps) => {
+
+    const handleSearch = (event) => {
+        if (event.key === 'Enter') {
+          const searchTerm = event.target.value;
+          //Faire quelque chose avec le terme de recherche (pour l'instant console.log)
+          console.log('Recherche: ' + searchTerm);
+        }
+      };
+    
+    return (
+        <div>
+            <input type='text' className='placeholder' placeholder={placeholder} onKeyDown={handleSearch}></input>
+            <svg href={srcIcon} className='vector'></svg>
+        </div>
+    )
+};
+
+export default InputSearch;

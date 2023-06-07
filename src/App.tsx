37a253da792cf9e3f5f516b8246dcb7f92312e0c
JulienBNT <julien.binet91@gmail.com>
2023-06-07 15:21:47 +0200

    :sparkles: export added to index.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import logo_popoyoko from "/Logo-popoyoko.svg";
 import "./App.css";
 import React from "react";
-import Card from "./components/Card";
 import Avatar from "./components/Avatar";
 
 function App() {
@@ -25,9 +24,9 @@ function App() {
         <Card background="url(https://imgs.search.brave.com/KUc_Sjgkt3SuouS7E-9zbhV2YGG7MFpPmcNklJmJYu0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9ib25q/b3VybmF0dXJlLmZy/L3dwLWNvbnRlbnQv/dXBsb2Fkcy9pbWFn/ZS1wYXlzYWdlLWdy/YXR1aXRlLWEtdGVs/ZWNoYXJnZXItNS5q/cGc)"/>
 
         <Card label="label" subTitle="subtitle" mention="mention" price={56} background="url(https://imgs.search.brave.com/KUc_Sjgkt3SuouS7E-9zbhV2YGG7MFpPmcNklJmJYu0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9ib25q/b3VybmF0dXJlLmZy/L3dwLWNvbnRlbnQv/dXBsb2Fkcy9pbWFn/ZS1wYXlzYWdlLWdy/YXR1aXRlLWEtdGVs/ZWNoYXJnZXItNS5q/cGc)"/> */}
-{/* <Avatar></Avatar>
+        {/* <Avatar></Avatar>
 <Avatar small></Avatar> */}
-<Avatar.Small showbadge background="red" />
+        <Avatar.Small showbadge background="red" />
       </div>
     </div>
   );

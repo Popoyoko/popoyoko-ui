9737916ea657dde41888dc72b72d0304ec61ed0b
Emerson452 <emerson452pro@gmail.com>
2023-06-08 10:01:01 +0200

    🎨 Change background string by url and default background

--- src/components/Avatar/Small/index.tsx
+++ src/components/Avatar/Small/index.tsx
@@ -7,9 +7,10 @@ interface SmallProps {
 }
 
 const Small = ({ showbadge, background }: SmallProps) => {
+  const backgroundImageStyle = background ? { backgroundImage: `url(${background})` } : {backgroundImage: `url(https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg)`};
 
   return (
-    <div className="small" style={{ background }}>
+    <div className="small" style={backgroundImageStyle}>
       {showbadge && <div className="badge"></div>}
     </div>
   );

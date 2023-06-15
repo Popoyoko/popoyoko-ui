c674bc24f43547a7fe3ff4251874897a32d53ff7
Emerson452 <emerson452pro@gmail.com>
2023-06-15 13:11:54 +0200

    🚚 Rename section svg by upload

--- src/stories/Icons/Upload/uploadIcon.tsx
+++ src/stories/Icons/Upload/uploadIcon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Upload from "../../../Icons/Upload/section.svg";
+import Upload from "../../../Icons/Upload/upload.svg";
 
 export const UploadIcon = () => {
   return <img src={Upload} alt="Upload" />;

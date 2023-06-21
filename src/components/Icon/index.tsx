82a5649d2278dbd382cdd366899d10f73550e1ac
Emerson452 <emerson452pro@gmail.com>
2023-06-21 10:35:42 +0200

    🚚 Add all svg path in icon component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,8 +1,21 @@
 import "./index.css";
 import React from "react";
 
-import addSvg from "../../Icons/Add/add.svg";
-import addPictureSvg from "../../Icons/AddPicture/addPicture.svg";
+import AddSvg from "../../Icons/Add/add.svg";
+import AddPictureSvg from "../../Icons/AddPicture/addPicture.svg";
+import ArrowBottom from "../../Icons/ArrowBottom/bottom.svg";
+import ArrowLeft from "../../Icons/ArrowLeft/left.svg";
+import ArrowRight from "../../Icons/ArrowRight/right.svg";
+import ArrowTop from "../../Icons/ArrowTop/top.svg";
+import ArrowTriangle from "../../Icons/ArrowTriangle/triangle.svg";
+import DateFlat from "../../Icons/DateFlat/dateFlat.svg";
+import DateLine from "../../Icons/DateLine/dateLine.svg";
+import Information from "../../Icons/Information/information.svg";
+import Search from "../../Icons/Search/search.svg";
+import Upload from "../../Icons/Upload/upload.svg";
+import UserFlat from "../../Icons/UserFlat/userFlat.svg";
+import UserLine from "../../Icons/UserLine/userLine.svg";
+
 import { ReactSVG } from 'react-svg';
 // import { ReactComponent as Add } from "react";
 
@@ -17,8 +30,8 @@ const Icon = ({ icon, size }: IconProps) => {
 
   return (
     <div className={className}>
-    {icon === "add" && <ReactSVG src={addSvg} />}
-    {icon === "addPicture" && <ReactSVG src={addPictureSvg} />}
+    {icon === "add" && <ReactSVG src={Add} />}
+    {icon === "addPicture" && <ReactSVG src={AddPicture} />}
   </div>
  
   );

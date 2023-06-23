ed4bcadf768d4e65d88d1e9d4210683fd50a520c
Emerson452 <emerson452pro@gmail.com>
2023-06-23 11:42:49 +0200

    🚚 Improve path of index.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,18 +1,12 @@
-
-export { default as Button } from "./Button"
-export { default as Ratio } from "./Ratio"
-export {default as InputSearch } from "./Inputs/Search";
-export { default as InputText } from "./Inputs/Text";
-
-// export { default as InputDownload } from "./Inputs/Download";
-
-
-export { default as Card } from "./Card";
-// export { default as Information } from "./Information";
-// export { default as Text } from "./Text";
-// export { default as Logo} from "./Logo";
-// export { default as Icon} from "./Icon";
 export { default as Avatar} from "./Avatar";
-// export { default as Section } from "./Section";
-// export { default as RatioRectangle } from "./Ratio/L2H3";
-// export { default as RatioCube } from "./Ratio/L1H1";
+export { default as Button } from "./Button"
+export { default as Card } from "./Card";
+export { default as Inputs } from "./Card";
+export { default as Ratio } from "./Ratio"
+
+export { Header } from "./Header"
+export { Icon } from "./Icon";
+export { Information } from "./Information";
+export { Logo } from "./Logo";
+export { Section } from "./Section";
+export { Text } from "./Text";

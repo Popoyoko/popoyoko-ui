0780e33c907a4852bc4be678ab30896d07e8bee1
LouiFi <louis@popoyoko.fr>
2023-06-23 14:29:14 +0200

    🚧

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,38 @@
-import * as path from 'path';
-import { defineConfig } from 'vite';
-import ts from 'rollup-plugin-typescript2';
-import eslint from '@rollup/plugin-eslint';
-import svgr from 'vite-plugin-svgr'
+import * as path from "path";
+import { defineConfig } from "vite";
+import ts from "rollup-plugin-typescript2";
+import eslint from "@rollup/plugin-eslint";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, './src/index.tsx'),
-      name: 'popoyoko-ui-vite',
-      formats: ['es', 'cjs'],
-      fileName: (format) => `popoyoko-ui-vite.${format}.js`
+      entry: path.resolve(__dirname, "./src/index.tsx"),
+      name: "popoyoko-ui-vite",
+      formats: ["es", "cjs"],
+      fileName: (format) => `popoyoko-ui-vite.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
       },
       plugins: [
         ts({
-          tsconfig: path.resolve(__dirname, 'tsconfig.json')
+          tsconfig: path.resolve(__dirname, "tsconfig.json"),
         }),
         eslint({
-          include: ['src/**/*.ts', 'src/**/*.tsx'], // Les fichiers à linter
-          exclude: ['node_modules/**', 'build/**'] // Les fichiers à exclure
+          include: ["src/**/*.ts", "src/**/*.tsx"], // Les fichiers à linter
+          exclude: ["node_modules/**", "build/**"], // Les fichiers à exclure
         }),
-        svgr()
-      ]
+        svgr(),
+      ],
     },
-    outDir: 'build',
+    outDir: "build",
     sourcemap: true,
-    assetsDir: 'assets'
-  }
+    assetsDir: "assets",
+  },
 });

8dfea3889d26a9247104deb6257faca6f2051656
Emerson452 <emerson452pro@gmail.com>
2023-07-31 12:25:58 +0200

    💡 Comment for react-svg types file

--- react-svg.d.ts
+++ react-svg.d.ts
@@ -1,3 +1,5 @@
+// fichier de déclaration de modules TypeScript (.d.ts) qui permet de définir manuellement les types pour la bibliothèque react-svg
+
 declare module "react-svg" {
     import { SVGProps } from "react";
   

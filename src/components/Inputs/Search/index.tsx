ff08cb6a22884984c374b46760bda4c11e06fd6e
Emerson452 <emerson452pro@gmail.com>
2023-07-31 12:57:02 +0200

    🐛 Remove console log in search input

--- src/components/Inputs/Search/index.tsx
+++ src/components/Inputs/Search/index.tsx
@@ -10,17 +10,19 @@ export const InputSearch = ({
     placeholder,
 }: InputSearchProps) => {
 
-    const handleSearch = (event) => {
-        if (event.key === 'Enter') {
-          const searchTerm = event.target.value;
-          //Faire quelque chose avec le terme de recherche (pour l'instant console.log)
-          console.log('Recherche: ' + searchTerm);
-        }
-      };
+    // const handleSearch = (event) => {
+    //     if (event.key === 'Enter') {
+    //       const searchTerm = event.target.value;
+    //       //Faire quelque chose avec le terme de recherche (pour l'instant console.log)
+    //       console.log('Recherche: ' + searchTerm);
+    //     }
+    //   };
+
+    // onKeyDown={handleSearch}
     
     return (
         <div className="input">
-            <input type='search' className='placeholder' placeholder={placeholder} onKeyDown={handleSearch}></input>
+            <input type='search' className='placeholder' placeholder={placeholder}></input>
             <Icon.Search/>
         </div>
     )

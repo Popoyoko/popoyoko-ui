28c947c4b577bbb333fbd83dcae625ab1507617b
Emerson452 <emerson452pro@gmail.com>
2023-08-24 11:00:53 +0200

    Remove node_modules from repository

--- node_modules/@storybook/manager-api/src/typings.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-underscore-dangle, @typescript-eslint/naming-convention */
-declare var __STORYBOOK_ADDONS_MANAGER: any;
-
-declare var CONFIG_TYPE: string;
-declare var FEATURES: import('@storybook/types').StorybookConfig['features'];
-declare var REFS: any;
-declare var VERSIONCHECK: any;
-declare var LOGLEVEL: 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'silent' | undefined;

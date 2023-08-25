8504ae38e71ca1973ff70246f777826ebd9a1241
Emerson452 <emerson452pro@gmail.com>
2023-08-25 11:57:46 +0200

    💄 css-in-js for Input Text component

--- src/components/Inputs/Text/index.tsx
+++ src/components/Inputs/Text/index.tsx
@@ -1,16 +1,35 @@
-import './index.css';
 import React from 'react';
+import styled from 'styled-components';
 
 interface InputTextProps {
     placeholder: string;
 };
 
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 4px;
+  padding: 12px;
+  background: #F7F7F8;
+  border: 2px solid #0F082B;
+  border-radius: 4px;
+`;
+
+const Placeholder = styled.p`
+  font-family: 'Co Headline';
+  font-style: normal;
+  font-size: 16px;
+  color: #0F082B;
+  margin: 0;
+`;
+
 export const InputText = ({
     placeholder,
 }: InputTextProps) => {
     return (
-        <div className='input'>
-            <p className='placeholder'>{placeholder}</p>
-        </div>
+        <InputWrapper>
+            <Placeholder>{placeholder}</Placeholder>
+        </InputWrapper>
     )
 }

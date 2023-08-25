8f1501e78e315dffde708e0258adf8d1567d6d97
Emerson452 <emerson452pro@gmail.com>
2023-08-25 11:30:20 +0200

    💄 css-in-js for Input Date component

--- src/components/Inputs/Date/index.tsx
+++ src/components/Inputs/Date/index.tsx
@@ -1,21 +1,47 @@
-import './index.css';
 import React from 'react';
+import styled from 'styled-components';
 
 interface InputDateProps {
-    placeholder: string;
-    srcIcon: string;
-    onClick?: () => void;
-};
+  placeholder: string;
+  srcIcon: string;
+  onClick?: () => void;
+}
+
+const DateButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 12px;
+  gap: 4px;
+  width: 326px;
+  height: 48px;
+  background: #f7f7f8;
+  border: 2px solid #0f082b;
+  border-radius: 4px;
+`;
+
+const Placeholder = styled.p`
+  box-sizing: border-box;
+`;
+
+const Vector = styled.svg`
+  position: absolute;
+  left: 3px;
+  right: 4px;
+  top: 2px;
+  bottom: 4px;
+`;
 
 export const InputDate = ({
-    placeholder,
-    srcIcon,
-    onClick,
+  placeholder,
+  srcIcon,
+  onClick,
 }: InputDateProps) => {
-    return (
-        <button className='dateBtn' onClick={onClick}>
-            <p className='placeholder'>{placeholder}</p>
-            <svg className='vector' href={srcIcon}></svg>
-        </button>
-    );
-}
+  return (
+    <DateButton onClick={onClick}>
+      <Placeholder>{placeholder}</Placeholder>
+      <Vector href={srcIcon} />
+    </DateButton>
+  );
+};

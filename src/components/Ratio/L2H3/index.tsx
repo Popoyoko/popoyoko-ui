60f6739423b19ba3b58eaaaa4613871e312cc647
Emerson452 <emerson452pro@gmail.com>
2023-08-25 12:13:07 +0200

    💄 css-in-js for Ratio L2H3 component

--- src/components/Ratio/L2H3/index.tsx
+++ src/components/Ratio/L2H3/index.tsx
@@ -1,8 +1,17 @@
-import "./index.css";
 import React from "react";
+import styled from 'styled-components';
+
+const RatioWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+`;
+
+const Ratio2to3 = styled(RatioWrapper)`
+  aspect-ratio: 3 / 2;
+`;
 
 export const L2H3 = ({ children }) => {
-  return <div className="ratio ratio-2-3">{children}</div>;
+  return <Ratio2to3>{children}</Ratio2to3>;
 };
 
 L2H3.defaultProps = {

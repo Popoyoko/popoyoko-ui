e7b5a535b18d08215a7a14b32f6a6f686b3a67d9
Emerson452 <emerson452pro@gmail.com>
2023-08-25 12:18:37 +0200

    💄 css-in-js for Text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,17 +1,44 @@
-import "./index.css";
 import React from "react";
+import styled from 'styled-components';
 
 interface TextProps {
   title: string;
   description: string;
 }
 
+const TextLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  overflow-y: scroll;
+  width: 19.5rem;
+  height: 18rem;
+`;
+
+const TextTitle = styled.p`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 32px;
+`;
+
+const TextDescription = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 16px 0px;
+  isolation: isolate;
+  font-family: 'sans-serif';
+  text-align: start;
+`;
+
 export const Text = ({ title, description}: TextProps) => {
   return (
-    <div className="text-layout">
-      <p className="text-title">{title}</p>
-      <p className="text-description">{description}</p>
-    </div>
+    <TextLayout>
+      <TextTitle>{title}</TextTitle>
+      <TextDescription>{description}</TextDescription>
+    </TextLayout>
   );
 };
 

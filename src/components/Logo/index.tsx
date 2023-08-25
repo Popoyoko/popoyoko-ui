8ad6aa1fdaf3cd5ae2a527a052258a88d3dcddb4
Emerson452 <emerson452pro@gmail.com>
2023-08-25 12:03:42 +0200

    💄 css-in-js for Logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,16 +1,28 @@
-import "./index.css";
 import React from 'react';
+import styled from 'styled-components';
 
 interface LogoProps {
     src: string;
 }
 
+const LogoWrapper = styled.div`
+  height: 32px;
+  width: 32px;
+  background: #D9D9D9;
+  display: flex;
+`;
+
+const LogoImage = styled.img`
+  max-height: 32px;
+  max-width: 32px;
+`;
+
 export const Logo = ({
     src
 }: LogoProps) => {
     return (
-        <div className="logo">
-            <img src={src}></img>
-        </div>
+        <LogoWrapper>
+            <LogoImage src={src}></LogoImage>
+        </LogoWrapper>
     );
 };

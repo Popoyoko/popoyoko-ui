66981b36c678b89ffc32acc967f6e973abc50dcd
Emerson452 <emerson452pro@gmail.com>
2023-08-29 23:17:21 +0200

    🚧 Some test for practolib dev

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,9 @@ import { useState } from "react";
 import logo_popoyoko from "/Logo-popoyoko.svg";
 import "./App.css";
 import React from "react";
-// import Card from "./components/Card";
-import Avatar from "./components/Avatar";
-
-import { Information } from "./components/Information";
-import { Icon } from "./components/Icon";
-
-import Button from "./components/Button";
-import { Header } from "./components";
-// import { Search } from "./Icons";
-import { InputDate } from "./components/Inputs/Date";
-import { InputText } from "./components/Inputs/Text";
+import { InputText } from "./components";
+import { Button } from "./components";
+import { Ratio } from "./components";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,40 +16,21 @@ function App() {
           <img src={logo_popoyoko} className="logo popoyoko" />
         </a>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
 
-        {/* <Button label="Click me!" onClick={() => alert('Hello world!')} backgroundColor="#f00"></Button>
+      {/* Practolib test */}
+      <Ratio.L1H1>
+        <InputText placeholder="placeholder" />
 
-        <Card background="url(https://imgs.search.brave.com/KUc_Sjgkt3SuouS7E-9zbhV2YGG7MFpPmcNklJmJYu0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9ib25q/b3VybmF0dXJlLmZy/L3dwLWNvbnRlbnQv/dXBsb2Fkcy9pbWFn/ZS1wYXlzYWdlLWdy/YXR1aXRlLWEtdGVs/ZWNoYXJnZXItNS5q/cGc)"/>
+        <InputText placeholder="placeholder" />
 
-        <Card label="label" subTitle="subtitle" mention="mention" price={56} background="url(https://imgs.search.brave.com/KUc_Sjgkt3SuouS7E-9zbhV2YGG7MFpPmcNklJmJYu0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9ib25q/b3VybmF0dXJlLmZy/L3dwLWNvbnRlbnQv/dXBsb2Fkcy9pbWFn/ZS1wYXlzYWdlLWdy/YXR1aXRlLWEtdGVs/ZWNoYXJnZXItNS5q/cGc)"/> */}
-        {/* <Avatar></Avatar>²
-<Avatar small></Avatar> */}
-        <Avatar.Small showbadge />
-        <Icon.Add />
-        <Icon.AddPicture size="small" />
-        <Icon.ArrowBottom size="large" />
-
-        <Information></Information>
-        <Avatar.Cover showedit/>
-<Information></Information>
-<Button.Primary label="Primary Button" typeSvg="none"/>
-<Button.Secondary label="Primary Button" typeSvg="none"/>
-<Button.Tertiary label="Primary Button" typeSvg="none"/>
-<Header></Header>
-        {/* <Icons.Information></Icons.Information> doesnt work*/}
-        <InputDate
-          placeholder="Saisissez une date"
-          srcIcon="chemin/vers/mon-icon.svg"
-          onClick={() => {
-            // Gérer l'événement onClick ici
-          }}
+        <Button.Primary
+          label="Se connecter"
+          onClick={() => {}}
+          typeSvg="none"
         />
-        <InputText placeholder="test"></InputText>
-      </div>
+
+        <Button.Tertiary label="Créer un compte" typeSvg="none" />
+      </Ratio.L1H1>
     </div>
   );
 }

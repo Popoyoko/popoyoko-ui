225659501598cc8cc73af93f8c3dd452b34d5a50
Emerson452 <emerson452pro@gmail.com>
2023-09-14 13:26:23 +0200

    🎨 Refacto information component

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -6,54 +6,33 @@ interface InformationProps {
   label?: string;
 }
 
-const ContainerInformation = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 32px;
-  gap: 32px;
-  position: relative;
-  width: calc(100% - 64px);
-  width: 20rem;
-  background: #ffffff;
-`;
-
-const Heading = styled.h2`
-  margin: 0;
-  font-family: "Co Headline";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 32px;
-  line-height: 24px;
-  color: #1d2525;
-`;
-
 const InformationSection = styled.div`
   display: flex;
-  flex-direction: row;
-  align-items: flex-start;
   padding: 16px;
+  align-items: flex-start;
   gap: 8px;
-  height: 100%;
-  width: 100%;
-  box-sizing: border-box;
-  background: #32568f;
-  box-shadow: 0px 4px 8px -4px rgba(90, 125, 129, 0.05);
+
   border-radius: 2px;
+  background: #2018b0;
+  box-shadow: 0px 4px 8px -4px rgba(84, 81, 121, 0.05);
 `;
 
-const InformationContent = styled.div`
+const InformationContent = styled.p`
   word-wrap: break-word;
+  color: #f0e8ff;
+  font-family: Co Headline;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  margin: 0;
 `;
 
 export const Information = ({ label = "Information" }: InformationProps) => {
   return (
-    <ContainerInformation>
-      <Heading>Information</Heading>
-      <InformationSection>
-        <Icon.Information className="information-svg" />
-        <InformationContent>{label}</InformationContent>
-      </InformationSection>
-    </ContainerInformation>
+    <InformationSection>
+      <Icon.Information className="information-svg" />
+      <InformationContent>{label}</InformationContent>
+    </InformationSection>
   );
 };

4e0bb2055ad7fea993c9175bd3594f6f4d0d4392
Emerson452 <emerson452pro@gmail.com>
2023-09-25 15:03:31 +0200

    ✨ Trying globalcss

---
+++ src/Theme.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: "Co Headline";
+    src: url("/fonts/Co_Headline_Bold.otf") format("otf");
+  }
+`;
+

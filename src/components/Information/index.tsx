d5dae5578b104ea3a7c4afcd05b670560caab539
Emerson452 <emerson452pro@gmail.com>
2023-09-29 15:59:37 +0200

    ✨ setting up the popoyoko token and changing the style of the information component

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -1,42 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 import { Icon } from "../Icon";
+import popoyoko from "../../tokens/popoyoko.json"
 
 interface InformationProps {
   label?: string;
   background?: string;
 }
 
-const InformationSection = styled.div`
-  display: flex;
-  padding: 16px;
-  align-items: flex-start;
-  gap: 8px;
-
-  border-radius: 2px;
-  background: #2018b0;
-  box-shadow: 0px 4px 8px -4px rgba(84, 81, 121, 0.05);
-`;
-
-const InformationContent = styled.p`
-  word-wrap: break-word;
-  color: #f0e8ff;
-  font-family: Co Headline;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 20px;
-  margin: 0;
-`;
-
-export const Information = ({ label = "Information", background = "#2018b0" }: InformationProps) => {
-  const InformationSectionWithBackground = styled(InformationSection)`
-  background: ${background};
-  `
+export const Information = ({ label = "Information", background = popoyoko.InformationSection.background }: InformationProps) => {
   return (
-    <InformationSectionWithBackground>
+    <div style={popoyoko.InformationSection}>
       <Icon.Information/>
-      <InformationContent>{label}</InformationContent>
-    </InformationSectionWithBackground>
+      <p style={popoyoko.InformationContent}>{label}</p>
+    </div>
   );
 };

dceabb9bd1f6b7c83a17a1fe82218efbdb22f156
Emerson452 <emerson452pro@gmail.com>
2023-10-09 15:20:38 +0200

    🎨 Adding ...props in InputSearch

--- src/components/Inputs/Search/index.tsx
+++ src/components/Inputs/Search/index.tsx
@@ -8,36 +8,35 @@ interface InputSearchProps {
 }
 
 const SearchWrapper = styled.div`
-display: flex;
-padding: 12px;
-justify-content: space-between;
-align-items: flex-start;
-gap: 4px;
+  display: flex;
+  padding: 12px;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 4px;
 
-border-radius: 4px;
-border: 2px solid #0F082B;
-box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  border: 2px solid #0f082b;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 `;
 
 const SearchInput = styled.input`
-color: #0F082B;
-font-family: Co Headline;
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: 24px; /* 150% */
+  color: #0f082b;
+  font-family: Co Headline;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px; /* 150% */
 
-border: none;
+  border: none;
 `;
 
-export const InputSearch = ({
-    placeholder,
-}: InputSearchProps) => {
-    
-    return (
-        <SearchWrapper>
-            <SearchInput type='search' placeholder={placeholder}></SearchInput>
-            <Icon.Search/>
-        </SearchWrapper>
-    )
-}
+export const InputSearch: React.FC<
+  InputSearchProps & React.HTMLProps<HTMLInputElement>
+> = ({ placeholder, ...props }: InputSearchProps) => {
+  return (
+    <SearchWrapper {...props}>
+      <SearchInput type="search" placeholder={placeholder}></SearchInput>
+      <Icon.Search />
+    </SearchWrapper>
+  );
+};

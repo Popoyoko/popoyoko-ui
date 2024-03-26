115cb561ecc616f20ad86fe051504d451be00d0b
Emerson452 <93705484+Emerson452@users.noreply.github.com>
2024-03-26 11:23:32 +0100

    💄 Change the ui of storybook (#213)
    * 🚚 Rename popoyoko assets file
    
    * 💄 Change ui of storybook

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,5 +1,6 @@
 import { addons } from '@storybook/manager-api';
 import { create } from '@storybook/theming';
+import './storybook-custom.css';
 
 addons.setConfig({
     theme: create({ 
@@ -8,5 +9,20 @@ addons.setConfig({
         brandUrl: "https://github.com/Popoyoko/popoyoko-ui",
         brandImage: "/logo.svg",
         brandTarget: '_self',
+
+        colorPrimary: '#191CB1',
+        colorSecondary: '#0023FC',   
+        
+        appBg: '#080312',
+        appPreviewBg: '#080312',
+
+        
+
+        textColor: '#9E89DA',
+        textInverseColor: '#ffffff',
+
+        barBg: '#080312',
+        barTextColor: '#191CB1',
+        barSelectedColor: '#0023FC',
     }),
 });

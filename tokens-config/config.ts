cd35c25982c013816037c10bbae95c84d10d1247
Emerson452 <emerson@popoyoko.fr>
2024-05-22 13:00:38 +0200

    🎨 Change the config for consumption

--- tokens-config/config.ts
+++ tokens-config/config.ts
@@ -1,9 +1,17 @@
-const basepath = "Popoyoko";
+const BrandName = "Popoyoko";
 
-const tokens = {
-import "./Component{basepath}.json";
-import "./Sizes{basepath}.json";
-}
+const FigmaComponentToken = import(`../build-tokens/Figma/Component${BrandName}.json`);
+const FigmaSizesToken = import(`../build-tokens/Figma/Sizes${BrandName}.json`);
+const WebColorTokenDark = import(`../build-tokens/web/Color${BrandName}Dark`);
+const WebColorTokenLight = import(`../build-tokens/web/Color${BrandName}Light`);
+const WebComponentToken = import(`../build-tokens/web/Component${BrandName}`);
+const WebSizesToken = import(`../build-tokens/web/Sizes${BrandName}`);
 
-
-export tokens;
+export {
+    FigmaComponentToken,
+    FigmaSizesToken,
+    WebColorTokenDark,
+    WebColorTokenLight,
+    WebComponentToken,
+    WebSizesToken
+};

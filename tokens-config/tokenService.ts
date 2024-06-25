4f3144dce584c4bfbbea13256ada05b6fc090b9b
Emerson452 <emerson@popoyoko.fr>
2024-06-25 14:36:03 +0200

    ✅ adjust config with some test

--- tokens-config/tokenService.ts
+++ tokens-config/tokenService.ts
@@ -2,17 +2,25 @@ interface TokenConfig {
     brand: string; 
   }
   
-  export const loadTokens = (config: TokenConfig) => {
+  export const loadTokens = async (config: TokenConfig) => {
     const { brand } = config;
   
     const pathToComponentTokens = `../build-tokens/web/Component${brand}.ts`;
     const pathToSizeTokens = `../build-tokens/web/Sizes${brand}.ts`;
   
-    const componentTokens = require(pathToComponentTokens);
-    const sizeTokens = require(pathToSizeTokens);
+    console.log("Loading tokens from:", pathToComponentTokens, pathToSizeTokens);
+
+    //const componentTokens = require(pathToComponentTokens);
+    //const sizeTokens = require(pathToSizeTokens);
+
+    const componentTokensModule = await import(pathToComponentTokens);
+    const sizeTokensModule = await import(pathToSizeTokens);
   
+    console.log("Loaded component tokens:", componentTokensModule.default);
+    console.log("Loaded size tokens:", sizeTokensModule.default);
+
     return {
-      componentTokens,
-      sizeTokens,
+        componentTokens: componentTokensModule.default,
+        sizeTokens: sizeTokensModule.default,
     };
   };

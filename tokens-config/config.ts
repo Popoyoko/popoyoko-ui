884371be4d8290618e1e60ae5f319c2d5d5e1800
Emerson452 <emerson@popoyoko.fr>
2024-06-25 10:52:22 +0200

    🎨 change the export tokens methode

--- tokens-config/config.ts
+++ tokens-config/config.ts
@@ -1,17 +1,1 @@
-const BrandName = "Popoyoko";
-
-const FigmaComponentToken = import(`../build-tokens/Figma/Component${BrandName}.json`);
-const FigmaSizesToken = import(`../build-tokens/Figma/Sizes${BrandName}.json`);
-const WebColorTokenDark = import(`../build-tokens/web/Color${BrandName}Dark`);
-const WebColorTokenLight = import(`../build-tokens/web/Color${BrandName}Light`);
-const WebComponentToken = import(`../build-tokens/web/Component${BrandName}`);
-const WebSizesToken = import(`../build-tokens/web/Sizes${BrandName}`);
-
-export {
-    FigmaComponentToken,
-    FigmaSizesToken,
-    WebColorTokenDark,
-    WebColorTokenLight,
-    WebComponentToken,
-    WebSizesToken
-};
+export const tokenPath = '../build-tokens/web';
--- tokens-config/tokensconfig.ts
+++ tokens-config/tokensconfig.ts
@@ -1,1 +1,12 @@
-export const tokenPath = '../build-tokens/web';
+const BrandName = "Popoyoko";
+
+const WebColorTokenDarkPromise = import(`../build-tokens/web/Color${BrandName}Dark.ts`);
+const WebColorTokenLightPromise = import(`../build-tokens/web/Color${BrandName}Light.ts`);
+const WebComponentTokenPromise = import(`../build-tokens/web/Component${BrandName}.ts`);
+const WebSizesTokenPromise = import(`../build-tokens/web/Sizes${BrandName}.ts`);
+
+
+export const WebColorTokenDark = () => WebColorTokenDarkPromise;
+export const WebColorTokenLight = () => WebColorTokenLightPromise;
+export const WebComponentToken = () => WebComponentTokenPromise;
+export const WebSizesToken = () => WebSizesTokenPromise;

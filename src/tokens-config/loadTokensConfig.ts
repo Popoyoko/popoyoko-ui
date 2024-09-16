4de415edca88537070efabb78b8d2fa369db48cc
Emerson452 <emerson@popoyoko.fr>
2024-09-16 11:18:30 +0200

    🐛 fix conflitcs

--- src/tokens-config/loadTokensConfig.ts
+++
@@ -1,8 +0,0 @@
-export const loadTokensConfig = async () => {
-    const defaultTokenPath = '../../popoyoko-default-variables/web/ComponentPopoyoko'; // Devrait être remplacé
-  
-    console.log('Final token path in browser:', defaultTokenPath);
-  
-    return defaultTokenPath;
-  };
-  
